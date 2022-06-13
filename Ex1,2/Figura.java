b567c21531d1da52c81a0b6ec993c095572f66b3
guilhermeken <103258737+guilhermeken@users.noreply.github.com>
2022-06-13 19:10:46 -0300

    Ex1, 2

---
+++ Ex1,2/Figura.java
@@ -0,0 +1,9 @@
+public abstract class Figura {
+    int quantPontos;
+    public int getQuantPontos(){
+        return quantPontos;
+    }
+    public void setQuantPontos(int quantPontos){
+        this.quantPontos = quantPontos;
+    }
+}
